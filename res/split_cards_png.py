10f4b3082901d6e17643430de27b5c163f176466
Denver Coneybeare <denver.coneybeare@gmail.com>
2012-04-03 03:00:47 -0400

    flatten directory structure

--- res/split_cards_png.py
+++ res/split_cards_png.py
@@ -1,23 +1,25 @@
 import subprocess
 
-ranks = ("ace", "king", "queen", "jack", 10, 9, 8, 7, 6, 5, 4, 3, 2)
-suits = ("spades", "hearts", "clubs", "diamonds")
+if __name__ == "__main__":
 
-y = 0
-for row in range(4):
-    x = 0
-    for col in range(13):
-        x_off = x * 216
-        y_off = y * 288
-        rank_name = ranks[x]
-        suit_name = suits[y]
-        geometry_str = "212x287+{}+{}".format(x_off, y_off)
-        filename = "card_{}_{}.png".format(suit_name, rank_name)
-        
-        args = ["cygconvert", "cards.png", "-crop", geometry_str, filename]
-        print(subprocess.list2cmdline(args))
-        subprocess.Popen(args).wait()
+    ranks = ("ace", "king", "queen", "jack", 10, 9, 8, 7, 6, 5, 4, 3, 2)
+    suits = ("spades", "hearts", "clubs", "diamonds")
+
+    y = 0
+    for row in range(4):
+        x = 0
+        for col in range(13):
+            x_off = x * 216
+            y_off = y * 288
+            rank_name = ranks[x]
+            suit_name = suits[y]
+            geometry_str = "212x287+{}+{}".format(x_off, y_off)
+            filename = "card_{}_{}.png".format(suit_name, rank_name)
+            
+            args = ["cygconvert", "cards.png", "-crop", geometry_str, filename]
+            print(subprocess.list2cmdline(args))
+            subprocess.Popen(args).wait()
 
 
-        x += 1
-    y += 1
+            x += 1
+        y += 1
